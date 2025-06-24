27ac5ff47d78a931df1749ac41fd2403e036cba2
lloyd konneker <konnekerl@gmail.com>
2025-06-24 09:41:09 -0400

    Delete old non max suppression op.

--- myGeglFilters/canny/nonMaxSuppressionOp/non-max-suppression.h
+++
@@ -1,9 +0,0 @@
-
-
-void
-non_maximum_suppression
- (GeglBuffer          *src,
-  const GeglRectangle *src_rect,
-  GeglBuffer          *dst,
-  const GeglRectangle *dst_rect,
-  const Babl          *format);
